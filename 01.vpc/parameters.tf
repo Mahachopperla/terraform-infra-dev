04157ec51de38fad0afdb80ddb6246efc6456914
maha <maridayya@gmail.com>
2025-06-26 12:10:32 +0530

    adding subent ids output

--- 01.vpc/parameters.tf
+++ 01.vpc/parameters.tf
@@ -4,10 +4,33 @@ resource "aws_ssm_parameter" "dev-vpc_id" {
   value = module.vpc.vpc_id
 }
 
-# we know aws follws strict naming convention for parametrs. they should be given with "/"
-resource "aws_ssm_parameter" "public_subnet_id's" {
-  name  = "/${var.main_project}/${var.main_environment}/Public_subnet_id's"
+# value get it from module-> module.vpc.Public_subent_id's
+# 						here we might get error because module gives us ouput as lsit of strings with braces
+# 							[ "public subnet 1",
+# 							"public subnet 2"]
+# 						but ssm parameter store accepts list of strings with comma(,) separated
+# 							public subnet1,public subnet 2
+# 						Now, we need to convert list into strings separated by commas
+# 							we should use join function
+# 							value = join(",", module.vpc.public_subnet_ids)
+resource "aws_ssm_parameter" "public_subnet_ids" {
+  name  = "/${var.main_project}/${var.main_environment}/public_subnet_ids"
   type  = "StringList"
-  value = module.vpc.public_subnet_ids
-  
+  value = join(",", module.vpc.public_subnet_ids)
+
 }
+
+resource "aws_ssm_parameter" "private_subnet_ids" {
+  name  = "/${var.main_project}/${var.main_environment}/private_subnet_ids"
+  type  = "StringList"
+  value = join(",", module.vpc.private_subnet_ids)
+
+}
+
+resource "aws_ssm_parameter" "database_subnet_ids" {
+  name  = "/${var.main_project}/${var.main_environment}/database_subnet_ids"
+  type  = "StringList"
+  value = join(",", module.vpc.database_subnet_ids)
+
+}
+
