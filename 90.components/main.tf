16c93e1a6f9bf6b60fb79cd8d7b525b50b5d25d7
maha <maridayya@gmail.com>
2025-07-10 16:03:23 +0530

    roboshop-infra

--- 90.components/main.tf
+++ 90.components/main.tf
@@ -1,5 +1,5 @@
 module "compoenents" {
-    source = "../../terraform-roboshop-module"
+    source = "git::https://github.com/Mahachopperla/terraform-roboshop-module.git?ref=main"
     for_each = var.components
     component = each.key
     rule_priority = each.value.rule_priority
